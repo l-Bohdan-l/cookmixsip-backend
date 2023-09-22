e27b05c5101689411862c3b367f05ea4daf41ec3
Bohdan <bpapushoj@gmail.com>
2023-09-22 17:41:21 +0300

    updt schema

--- routes/api/recipe-validation-schema.js
+++ routes/api/recipe-validation-schema.js
@@ -15,27 +15,31 @@ export const recipeValidationSchema = Joi.object({
   url: Joi.string().uri().messages({
     "string.uri": `Filed Url must be a valid URI`,
   }),
-  ingredients: Joi.object({
-    ingredientsName: Joi.string().alphanum().messages({
-      "string.alphanum":
-        "Filed IngredientsName can only contain a-z, A-Z, and 0-9",
-      "string.base": `Filed IngredientsName isn't a string`,
-    }),
-    ingredientsAmount: Joi.number().messages({
-      "number.base": `Filed IngredientsAmount isn't a number`,
-    }),
-    measure: Joi.string().alphanum().messages({
-      "string.alphanum": "Filed Measure can only contain a-z, A-Z, and 0-9",
-      "string.base": `Filed Measure isn't a string`,
-    }),
+  ingredients: Joi.array().items(
+    Joi.object({
+      ingredientsName: Joi.string().alphanum().messages({
+        "string.alphanum":
+          "Filed IngredientsName can only contain a-z, A-Z, and 0-9",
+        "string.base": `Filed IngredientsName isn't a string`,
+      }),
+      ingredientsAmount: Joi.number().messages({
+        "number.base": `Filed IngredientsAmount isn't a number`,
+      }),
+      measure: Joi.string().alphanum().messages({
+        "string.alphanum": "Filed Measure can only contain a-z, A-Z, and 0-9",
+        "string.base": `Filed Measure isn't a string`,
+      }),
+    })
+  ),
+  mealType: Joi.string().alphanum().min(4).max(8).required().messages({
+    "string.base": `Filed MealType isn't a string`,
+    "string.empty": `Filed MealType can't be empty`,
+    "any.required": `Filed MealType is required`,
+    "string.min": "Filed MealType must be at least 4 characters long",
+    "string.max": `Filed MealType must be at most 8 characters long`,
+    "string.alphanum": "Filed MealType can only contain a-z, A-Z, and 0-9",
   }),
-  mealType: Joi.string().alphanum().required().messages({
-    "string.base": `Filed Name isn't a string`,
-    "string.empty": `Filed Name can't be empty`,
-    "any.required": `Filed Name is required`,
-    "string.min": "Filed Name must be at least 2 characters long",
-    "string.max": `Filed Name must be at most 30 characters long`,
-    "string.alphanum": "Filed Name can only contain a-z, A-Z, and 0-9",
+  alcoholType: Joi.string().messages({
+    "string.base": `Filed description isn't a string`,
   }),
-  alcoholType: Joi.string().alphanum(),
 });
